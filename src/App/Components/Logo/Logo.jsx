b8d14596d69d37be97e5eb691f54dbe14da8503c
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-08-23 19:42:28 +0200

    Logo

---
+++ src/App/Components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+import logo from '../../Images/start-it-logo.svg';
+
+export const Logo = () => {
+  return (
+    <div>
+      <img src={logo} />
+    </div>
+  );
+};
--- src/App/Exercises/Js/Exercise-js-functions/index.jsx
+++ src/App/Exercises/Js/Exercise-js-functions/index.jsx
@@ -1,12 +1,25 @@
 import './styles.css';
 
 export function Exercise() {
+
+const animal = {
+  name: 'dog',
+  age: 15
+}
+animal.age = 60;
+console.log(animal.age);
+
+
+
+  const numer1 = 4;
+  const numer2 = 5;
+
   function multiply(num1, num2) {
     return num1 * num2;
   }
 
   function getWelcomeMessage(name, amount) {
-    return `Witaj ${name}! Jesteś ${amount} uzytkownikiem odwiedzającym tę stronę!`;
+    return `Witaj ${name}! Jesteś ${amount} użytkownikiem odwiedzającym tę stronę!`;
   }
 
   return (
@@ -33,6 +46,12 @@ export function Exercise() {
           <code>{getWelcomeMessage('Jon', 129384)}</code>
         </pre>
       </section>
+      <pre>
+        <code>4 * 5 = {multiply(numer1, numer2)}</code>
+      </pre>
+
+      <p>Wynik funkcji {multiply(numer1, numer2)}</p>
+      <p> {getWelcomeMessage('Ola', 13)}</p>
     </div>
   );
 }
