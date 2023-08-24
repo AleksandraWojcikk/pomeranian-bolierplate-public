3009c3daa084c1f572854b9cd8de6c22e7a882b2
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-08-24 20:37:16 +0200

    onclick with count

--- src/App/Components/ReactOnClick/ReactOnClick.jsx
+++ src/App/Components/ReactOnClick/ReactOnClick.jsx
@@ -2,18 +2,20 @@ import './styles.css';
 import { useState } from 'react';
 
 export const ReactOnCLick = () => {
-  const [isClicked, setIsClicked] = useState(false);
+  //const [isClicked, setIsClicked] = useState(false);
+  const [count, setIsCount] = useState(0);
 
-  const text = isClicked ? 'Kliknięte' : 'Nie kliknięte';
+  //const text = isClicked ? 'Kliknięte' : 'Nie kliknięte';
 
   function handleCLick() {
-    setIsClicked(!isClicked);
+    //setIsClicked(!isClicked);
+    setIsCount(count + 1);
   }
 
   return (
     <div>
       <h1>Cześć!</h1>
-      <button onClick={handleCLick}>{text}</button>
+      <button onClick={handleCLick}>{count}</button>
     </div>
   );
 };
