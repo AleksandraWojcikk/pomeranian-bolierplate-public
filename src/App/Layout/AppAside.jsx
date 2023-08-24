24416190fdc730d07090bec5bbb5e44a58ec0a9a
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-08-24 13:02:14 +0200

    My CV page page

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -1,19 +1,30 @@
 import { NavLink } from 'react-router-dom';
 
 import './styles/aside.css';
+import { HouseIcon } from '../Components/Icons/HouseIcon';
+import { ElementIcon } from '../Components/Icons/ElementIcon';
+import { EditIcon } from '../Components/Icons/EditIcon';
+import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
 
 export function AppAside() {
   return (
     <aside>
       <nav>
         <ul>
-          <li>
+          <li className="aside-row">
+            <HouseIcon />
             <NavLink to="dashboard">Dashboard</NavLink>
           </li>
-          <li>
+          <li className="aside-row">
+            <PersonalCardIcon />
+            <NavLink to="CV">Moje CV</NavLink>
+          </li>
+          <li className="aside-row">
+            <ElementIcon />
             <NavLink to="blocks">Bloki</NavLink>
           </li>
-          <li>
+          <li className="aside-row">
+            <EditIcon />
             <NavLink to="exercises">Ćwiczenia</NavLink>
           </li>
         </ul>
