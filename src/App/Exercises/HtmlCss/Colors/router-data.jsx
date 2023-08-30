4634675210c23ca6118e01253495de6f05b096fb
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-08-30 18:40:03 +0200

    colors

---
+++ src/App/Exercises/HtmlCss/Colors/router-data.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Colors } from './Colors';
+
+export const colorsRouterMetaData = {
+  path: 'colors', //ściezka, jaką wpisujesz w przeglądarce
+  date: '30-08-2023',
+  linkLabel: 'Colors',
+  element: <Colors />,
+  tags: ['flex', 'grid', 'video'],
+};
