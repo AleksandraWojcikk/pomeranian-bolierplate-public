08361d4e4a8234ffb0905c87164b3efba2f9689e
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-08-30 12:42:46 +0200

    standardtags

---
+++ src/App/Exercises/HtmlCss/StandardTags/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { StandardTags } from './StandardTags';
+
+export const standardTagsRouterMetaData = {
+  path: 'standard-tags', //ściezka, jaką wpisujesz w przeglądarce
+  date: '29-08-2023',
+  linkLabel: 'Standard tags',
+  blockNo: 1,
+  element: <StandardTags />,
+  tags: ['flex', 'grid', 'video'],
+};
