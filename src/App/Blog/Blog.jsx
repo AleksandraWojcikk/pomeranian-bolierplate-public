7b6dda1aec40a8d32d2e6e929683eedb83b81513
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-08-31 14:25:42 +0200

    matryca1 tekst1

--- src/App/Blog/Blog.jsx
+++ src/App/Blog/Blog.jsx
@@ -2,27 +2,34 @@ import './styles.css';
 
 export const Blog = () => {
   return (
-    <div className="wrapper">
+    <>
       <div className="blog">BLOG</div>
-      <div className="date">29-08-2023</div>
-      <div className="title">
-        <strong>Why are we so nostalgic for the 1990s?</strong>
+      <div className="wrapper">
+        <div className="date">30-11-2022</div>
+        <h1>
+          <strong>Why are we so nostalgic for the 1990s?</strong>
+        </h1>
+        <div className="tekst">
+          <p>
+            Pop culture used to define a generation, but it seems the cultural,
+            music and fashion trends of the 1990s have been recycled, and what
+            Generation X considered its rite of passage into adulthood, is being
+            discovered and claimed by fledgling grown-ups as their own.
+          </p>
+          <p>
+            There's been the resurgence of vinyl as the trend-setters' choice of
+            music consumption rather than the ease of a digital download, and
+            now the hipsters have discovered the nostalgic sound of a whirring
+            cassette from which to enjoy the dulcet tones of everyone from
+            Salt-N-Pepa to Rick Astley.
+          </p>
+        </div>
+        <p className="name">
+          <strong>Radosława Majdan</strong>
+        </p>
+
+        <p className="specialist">Senior Marketing Specialist</p>
       </div>
-      <div className="tekst">
-        Pop culture used to define a generation, but it seems the cultural,
-        music and fashion trends of the 1990s have been recycled, and what
-        Generation X considered its rite of passage into adulthood, is being
-        discovered and claimed by fledgling grown-ups as their own. There's been
-        the resurgence of vinyl as the trend-setters' choice of music
-        consumption rather than the ease of a digital download, and now the
-        hipsters have discovered the nostalgic sound of a whirring cassette from
-        which to enjoy the dulcet tones of everyone from Salt-N-Pepa to Rick
-        Astley.
-      </div>
-      <div className="name">
-        <strong>Radosława Majdan</strong>
-      </div>
-      <div className="specialist">Senior Marketing Specialist</div>
-    </div>
+    </>
   );
 };
