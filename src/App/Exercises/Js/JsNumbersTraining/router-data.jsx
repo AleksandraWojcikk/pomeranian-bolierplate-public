70844d2ea982b1cc1d832f410a67a9946236255c
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-09-08 19:36:07 +0200

    math

---
+++ src/App/Exercises/Js/JsNumbersTraining/router-data.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { JsNumbersTraining } from './JsNumbersTraining';
+
+export const jsNumbersTrainingRouterMetaData = {
+  path: 'js-numbers-training',
+
+  date: '08-09-2023',
+
+  linkLabel: 'JS numbers training',
+
+  element: <JsNumbersTraining />,
+
+  tags: ['boolean', 'const', 'true', 'false'],
+};
---
+++ src/App/Exercises/Js/MathExercise/router-data.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { MathExercise } from './MathExercise';
+
+export const mathExerciseRouterMetaData = {
+  path: 'math',
+
+  date: '08-09-2023',
+
+  linkLabel: 'Matematyka',
+
+  element: <MathExercise />,
+
+  tags: ['boolean', 'const', 'true', 'false'],
+};
