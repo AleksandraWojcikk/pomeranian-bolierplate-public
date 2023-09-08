fc1265ec83f2e7c0fd5ea761cb1fc4bd9afa229c
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-09-08 17:34:20 +0200

    jsnumber

---
+++ src/App/Exercises/Js/JsNumbers/router-data.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { JsNumbers } from './JsNumbers';
+
+export const jsNumbersRouterMetaData = {
+  path: 'js-numbers',
+
+  date: '07-09-2023',
+
+  linkLabel: 'JS liczby',
+
+  element: <JsNumbers />,
+
+  tags: ['boolean', 'const', 'true', 'false'],
+};
