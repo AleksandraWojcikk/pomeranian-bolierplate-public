02d6e7120cc5b397f2b97dbf06e15d629270a0ef
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-09-12 09:19:57 +0200

    arraybasics

---
+++ src/App/Exercises/Js/JsArrayBasics/router-data.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { JsArrayBasics } from './JsArrayBasics';
+
+export const jsArrayBasicsRouterMetaData = {
+  path: 'js-array-basics',
+
+  date: '11-09-2023',
+
+  linkLabel: 'Podstawy tablic',
+
+  element: <JsArrayBasics />,
+};
