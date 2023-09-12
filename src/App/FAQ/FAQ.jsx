7db69ca46cda2382b9acad12e4b14c3bf9bb99d7
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-09-12 10:28:08 +0200

    adding the line

--- src/App/FAQ/FAQ.jsx
+++ src/App/FAQ/FAQ.jsx
@@ -5,11 +5,26 @@ export const FAQ = () => {
   return (
     <>
       <div>
-        <SingleQuestion question="Jak masz na imię" answer="Ola" />
-        <SingleQuestion question="Ile masz lat?" answer="46" />
-        <SingleQuestion question="Test 1" answer="Answer 1" />
-        <SingleQuestion question="Test 1" answer="Answer 1" />
-        <SingleQuestion question="Test 1" answer="Answer 1" />
+        <SingleQuestion
+          question="Jak mogę zapisać się na szkolenie?"
+          answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend rutrum ipsum, leo et in. Mattis porttitor volutpat placerat suspendisse ante in cursus etiam ullamcorper. Sollicitudin egestas aliquam, adipiscing adipiscing iaculis habitant. Viverra pretium tincidunt nisl pellentesque ut adipiscing non.Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        />
+        <SingleQuestion
+          question="Jak mogę zapisać się na szkolenie?"
+          answer="No answer yet."
+        />
+        <SingleQuestion
+          question="Czego oczekujesz po kursie?"
+          answer="Chcę znaleźć pracę w branży IT."
+        />
+        <SingleQuestion
+          question="Czy masz doświadczenie w branży IT?"
+          answer="Nie mam, dlatego jest mi tak trudno."
+        />
+        <SingleQuestion
+          question="Czym zajmujesz się na codzień?"
+          answer="Pracuję jako QA."
+        />
       </div>
     </>
   );
--- src/App/FAQ/SingleQuestion/SingleQuestion.jsx
+++ src/App/FAQ/SingleQuestion/SingleQuestion.jsx
@@ -3,11 +3,11 @@ import './styles.css';
 export const SingleQuestion = ({ question, answer }) => {
   return (
     <>
-      <div className="wrapper">
+      <div className="wrapperFaQ">
         <h1>
           <strong>{question}</strong>
         </h1>
-        <hr />
+        <hr></hr>
         <p>{answer}</p>
       </div>
     </>
