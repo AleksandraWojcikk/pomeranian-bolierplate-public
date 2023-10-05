708dd9d100e88353df1251b31cc8a6027f7ab43b
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-10-05 18:43:07 +0200

    promises

--- src/App/Exercises/Js/PromisesExercises2/PromisesExercises2.jsx
+++ src/App/Exercises/Js/PromisesExercises2/PromisesExercises2.jsx
@@ -1,27 +1,69 @@
+// dodajmy nowy div na wyświetlenie błędu
+
+// po kliknięciu w button załaduj detale użytkownika wyczyśćmy także aktualny błąd.
+
+// zmodyfikujmy loadUserDetails w taki sposób aby z 50% prawdopodobieństwem rejectowała promisa
+// z bledem
+
+// teść błędu: błąd server: nieudane wyszukanie dla userId: xxx użyjmy klasy Error
+
+// w funkcji loadAllUserData przechwycić błąd i wyświetlić
+
+// przetestować aż do uzyskania błędu (możemy skrócić czas timeoutów na czas testów)
+
+import { useState } from 'react';
+
 export const PromisesExercises2 = () => {
-  const myPromise = () =>
-    new Promise((resolve, reject) => {
+  const [data, setData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  function loadUser() {
+    return new Promise((resolve) => {
       setTimeout(() => {
-        const randomNumber = Math.round(Math.random());
-        if (randomNumber) {
-          resolve('Sekces');
+        resolve({ id: 1, name: 'John Doe' });
+      }, 2000);
+    });
+  }
+
+  function loadUserDetails(userId) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() < 0.5) {
+          reject(new Error('Nie udało się pobrać danych'));
         } else {
-          reject('Porazka');
+          resolve({ id: userId, age: 30, country: 'Poland' });
         }
       }, 2000);
     });
-
-  myPromise()
-    .then((value) => {
-      console.log(value);
-      return value;
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-    .finally(() => {
-      console.log('koniec');
+  }
+  //przy ładowaniu danych zawsze musza być stany wyzerowane setData(null)
+  function loadAllUserData() {
+    setData(null);
+    setErrorMessage(null);
+    loadUser().then((user) => {
+      loadUserDetails(user.id)
+        .then((details) => {
+          setData(details);
+        })
+        .catch((err) => {
+          setErrorMessage(err.message); //.message bo w 32. linijce uyliśmy new Error
+        });
     });
+  }
 
-  return <div>zmien mnie</div>;
+  return (
+    <>
+      <div>
+        <button onClick={loadAllUserData}>Załaduj detale użytkownika</button>
+        {data && (
+          <div>
+            <p>Id: {data.id}</p>
+            <p>Age: {data.age}</p>
+            <p>Country: {data.country}</p>
+          </div>
+        )}
+        {errorMessage && <div>{errorMessage}</div>}
+      </div>
+    </>
+  );
 };
